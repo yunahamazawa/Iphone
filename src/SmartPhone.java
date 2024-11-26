37f24056f48b4f2b7bbdfb978d89ad114bccae63
yunahamazawa <yunahamazawa821@gmail.com>
2024-11-26 21:21:10 +0900

    演習④

--- src/SmartPhone.java
+++ src/SmartPhone.java
@@ -1,6 +1,18 @@
 
 public class SmartPhone extends Mp3Player{
-	public void call(){
+	public void play(){
+		System.out.println("再生（ハイレゾ機能）");
+		}
+		public void stop(){
+		System.out.println("停止（ハイレゾ機能）");
+		}
+		public void next(){
+		System.out.println("次へ（ハイレゾ機能）");
+		}
+		public void back(){
+		System.out.println("戻る（ハイレゾ機能）");
+		}
+		public void call(){
 		System.out.println("電話");
 		}
 		public void mail(){
